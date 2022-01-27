db5bac2cb5559ef4a2593ca1c8ee3c9a3bc51683
Prasad Sanas <sanasprasad98@gmail.com>
2022-01-28 01:19:11 +0530

    Cryptocurrencies done

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,9 +3,16 @@ import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 
+import { useGetCryptosQuery } from "../services/cryptoApi";
+import { Cryptocurrencies, News } from "../components";
+
 const { Title } = Typography;
 
 const Homepage = () => {
+  const { data, isFetching } = useGetCryptosQuery(10);
+  const globatStats = data?.data?.stats;
+
+  if (isFetching) return "Loading...";
   return (
     <>
       <Title level={2} className="heading">
@@ -13,21 +20,51 @@ const Homepage = () => {
       </Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value={5} />
+          <Statistic title="Total Cryptocurrencies" value={globatStats.total} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value={5} />
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globatStats.totalExchanges)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value={5} />
+          <Statistic
+            title="Total Market Cap"
+            value={millify(globatStats.totalMarketCap)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value={5} />
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globatStats.total24hVolume)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value={5} />
+          <Statistic
+            title="Total Markets"
+            value={millify(globatStats.totalMarkets)}
+          />
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={5} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest News
+        </Title>
+        <Title level={5} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 };
