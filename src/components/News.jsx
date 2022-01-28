1586310bf3e2f9059d39413a990fa88f6eac3171
Prasad Sanas <sanasprasad98@gmail.com>
2022-01-28 19:15:49 +0530

    Crypto

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { Select, Row, Col, Avatar, Typography, Card } from "antd";
+import moment from "moment";
 
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 const News = () => {
   return <div>News</div>;
 };
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "x-bingapis-sdk": "true",
+  "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+  "x-rapidapi-key": "766f177417msh23170e2dd2a3a10p128835jsn9dc5c11a2ccd",
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
